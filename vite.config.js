3fc1e0e83e8e004e9a6b2cd3c2cfb0593aab4f52
Hugo Morales <hmoralesg@icloud.com>
2024-04-08 18:14:31 -0400

    added bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
   plugins: [vue()],
 })

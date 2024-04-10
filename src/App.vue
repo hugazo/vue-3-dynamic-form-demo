068daf36c56be925bd4b304dee721bdba00f4d55
Hugo Morales <hmoralesg@icloud.com>
2024-04-10 03:01:31 -0400

    feat: handles form mock submission

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,19 @@
 import FormField from '@/components/FormField.vue';
 import useForms from '@/composables/useForms';
 
-const { FormData, formValues } = useForms();
+// Imports useForms composable, handles business logic for form
+const {
+  FormData,
+  formValues,
+  valid,
+  formLoading,
+  handleFormSubmit
+} = useForms();
 </script>
 
 <template>
   <div class="container">
+    <!-- Iterates over FormData array and renders FormField component for each field -->
     <template
       v-for="field, index in FormData"
       :key="index"
@@ -18,5 +26,21 @@ const { FormData, formValues } = useForms();
         :field
       />
     </template>
+    <!-- Form Submit -->
+    <div class="form-group py-4">
+      <button
+        class="form-control btn btn-primary"
+        :disabled="!valid || formLoading"
+        @click="handleFormSubmit"
+      >
+        <span
+          v-if="formLoading"
+          class="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"
+        />
+        {{ formLoading ? 'Loading...' : 'Submit' }}
+      </button>
+    </div>
   </div>
 </template>
--- src/composables/useForms.js
+++ src/composables/useForms.js
@@ -2,7 +2,7 @@ import { computed, ref } from 'vue';
 import FormData from '@/data/form.json';
 
 export default () => {
-  const valid = ref(false);
+  const formLoading = ref(false);
 
   const formValues = ref(FormData.reduce((acc, field) => {
     // Declared as ref for two-way binding
@@ -14,6 +14,7 @@ export default () => {
     return acc;
   }, {}));
 
+  // Computed property that handles dynamic field validations from json and creates reactive object for component binding
   const handleValidField = (field, value) => {
     // Required field
     if (field.required && value.value.length === 0) {
@@ -56,9 +57,56 @@ export default () => {
     };
   };
 
+  const valid = computed(() => {
+    const values = Object.values(formValues.value);
+    if (!values.length) return false;
+    const validValues = values.every((field) => {
+      return field.result.valid ? true : false;
+    });
+    return validValues;
+  });
+
+  const handleFormSubmit = async () => {
+    formLoading.value = true;
+    try {
+      if (!valid.value) throw new Error('Form is invalid');
+      const body = Object.keys(formValues.value).reduce((acc, key) => {
+        acc[key] = formValues.value[key].value;
+        return acc;
+      }, {});
+      await executeMockApiCall(body)
+      cleanForm();
+      console.log('Form submitted successfully')
+    } catch (e) {
+      console.error('Error submitting form', e);
+    } finally {
+      formLoading.value = false;
+    }
+  };
+
+  const cleanForm = () => {
+    Object.keys(formValues.value).forEach((key) => {
+      formValues.value[key].value = '';
+    });
+  };
+
+  const executeMockApiCall = (body) => {
+    console.log('Api Call Begin', body)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // reject this a 20% of the time
+        if (Math.random() > 0.8) {
+          return reject('Error fetching data');
+        }
+        resolve('Data fetched');
+      }, Math.floor(Math.random() * 2000) + 2000);
+    });
+  }
 
   return {
     valid,
+    formLoading,
+    handleFormSubmit,
     FormData,
     formValues,
   }

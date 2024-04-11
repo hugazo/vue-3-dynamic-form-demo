5f3a148f407836efc76681d0dc6d3f1426576f42
Hugo Morales <hmoralesg@icloud.com>
2024-04-10 21:26:36 -0400

    feat: updated UI

--- src/App.vue
+++ src/App.vue
@@ -13,34 +13,47 @@ const {
 </script>
 
 <template>
-  <div class="container">
-    <!-- Iterates over FormData array and renders FormField component for each field -->
-    <template
-      v-for="field, index in FormData"
-      :key="index"
-    >
-      <FormField
-        v-model:value="formValues[field.name].value"
-        v-model:result="formValues[field.name].result"
-        class="py-2"
-        :field
-      />
-    </template>
-    <!-- Form Submit -->
-    <div class="form-group py-4">
-      <button
-        class="form-control btn btn-primary"
-        :disabled="!valid || formLoading"
-        @click="handleFormSubmit"
-      >
-        <span
-          v-if="formLoading"
-          class="spinner-border spinner-border-sm"
-          role="status"
-          aria-hidden="true"
-        />
-        {{ formLoading ? 'Loading...' : 'Submit' }}
-      </button>
+  <div class="main-container container-fluid d-flex justify-content-center align-items-center">
+    <div class="card">
+      <div class="card-body">
+        <!-- Iterates over FormData array and renders FormField component for each field -->
+        <template
+          v-for="field, index in FormData"
+          :key="index"
+        >
+          <FormField
+            v-model:value="formValues[field.name].value"
+            v-model:result="formValues[field.name].result"
+            class="py-2"
+            :field
+          />
+        </template>
+        <!-- Form Submit -->
+        <div class="form-group py-4">
+          <button
+            class="form-control btn btn-primary"
+            :disabled="!valid || formLoading"
+            @click="handleFormSubmit"
+          >
+            <span
+              v-if="formLoading"
+              class="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            />
+            {{ formLoading ? 'Loading...' : 'Submit' }}
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.main-container {
+  height: 100vh;
+}
+.card {
+  width: 400px;
+}
+</style>
